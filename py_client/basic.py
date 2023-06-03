e73307a0b7fef3a20470303815e369e4490057e8
Ved K <vedkorla300@gmail.com>
2023-06-03 21:47:13 +0530

    Created first API view

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,12 +1,12 @@
 import requests
 
-endpoint1 = "http://127.0.0.1:8000/"
+endpoint1 = "http://127.0.0.1:8000/api"
 
 
 response = requests.get(endpoint1)
 
 
-print(response.text)
+print(response.json()['Message'])
 
 
 # HTTP Request -> HTML
